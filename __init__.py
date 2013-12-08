60004e31b0acf95175d06750b604c7e51dee3ec6
Brian Wiborg <baccenfutter@c-base.org>
2013-12-08 17:00:37 +0100

    add docstrings

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
+"""FS-Hopper
+
+A thin and simplistic abstraction layer for accessing a filesystem
+directory tree in an object-oriented fashion.
+"""
+__author__ = "Brian Wiborg <baccenfutter@c-base.org>"
+__date__ = '2013-12-08'
+__license__ = 'public domain'
+
 from nodes import DirectoryNode as Directory
 from nodes import FileNode as File
 
